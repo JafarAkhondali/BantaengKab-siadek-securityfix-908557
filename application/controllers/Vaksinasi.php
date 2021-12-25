c549a0fb60258b8501e8591818a2c2c991c9f645
Muhammad Asdar MLD <muhammadasdar5@gmail.com>
2021-12-25 15:23:45 +0800

    crud vaksin

---
+++ application/controllers/Vaksinasi.php
@@ -0,0 +1,315 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/**
+*| --------------------------------------------------------------------------
+*| Vaksinasi Controller
+*| --------------------------------------------------------------------------
+*| Vaksinasi site
+*|
+*/
+class Vaksinasi extends Admin	
+{
+	
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('model_vaksinasi');
+	}
+
+	/**
+	* show all Vaksinasis
+	*
+	* @var $offset String
+	*/
+	public function index($offset = 0)
+	{
+		$this->is_allowed('vaksinasi_list');
+
+		$filter = $this->input->get('q');
+		$field 	= $this->input->get('f');
+
+		$this->data['vaksinasis'] = $this->model_vaksinasi->get($filter, $field, $this->limit_page, $offset);
+		$this->data['vaksinasi_counts'] = $this->model_vaksinasi->count_all($filter, $field);
+
+		$config = [
+			'base_url'     => 'vaksinasi/index/',
+			'total_rows'   => $this->model_vaksinasi->count_all($filter, $field),
+			'per_page'     => $this->limit_page,
+			'uri_segment'  => 3,
+		];
+
+		$this->data['pagination'] = $this->pagination($config);
+
+		$this->template->title('Vaksinasi List');
+		$this->render('modul/vaksinasi/vaksinasi_list', $this->data);
+	}
+	
+	/**
+	* Add new vaksinasis
+	*
+	*/
+	public function add()
+	{
+		$this->is_allowed('vaksinasi_add');
+
+		$this->template->title('Vaksinasi New');
+		$this->render('modul/vaksinasi/vaksinasi_add', $this->data);
+	}
+
+	/**
+	* Add New Vaksinasis
+	*
+	* @return JSON
+	*/
+	public function add_save()
+	{
+		if (!$this->is_allowed('vaksinasi_add', false)) {
+			echo json_encode([
+				'success' => false,
+				'message' => cclang('sorry_you_do_not_have_permission_to_access')
+				]);
+			exit;
+		}
+
+		$this->form_validation->set_rules('nik', 'Nik', 'trim|required|max_length[100]');
+		$this->form_validation->set_rules('nama', 'Nama', 'trim|required|max_length[100]');
+		$this->form_validation->set_rules('jenis_kelamin', 'Jenis Kelamin', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('kl_usia', 'Kelompok Usia', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('dosis', 'Dosis', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('jenis_vaksin', 'Jenis Vaksin', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('faskes', 'Faskes', 'trim|required|max_length[100]');
+		$this->form_validation->set_rules('tiket', 'Tiket', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('tanggal', 'Tanggal', 'trim|required');
+		
+
+		if ($this->form_validation->run()) {
+		
+			$save_data = [
+				'nik' => $this->input->post('nik'),
+				'nama' => $this->input->post('nama'),
+				'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+				'kl_usia' => $this->input->post('kl_usia'),
+				'dosis' => $this->input->post('dosis'),
+				'jenis_vaksin' => $this->input->post('jenis_vaksin'),
+				'faskes' => $this->input->post('faskes'),
+				'tiket' => $this->input->post('tiket'),
+				'tanggal' => $this->input->post('tanggal'),
+				'created_by' => get_user_data('username'),
+				'creation_date' => date('Y-m-d H:i:s'),
+			];
+
+			
+			$save_vaksinasi = $this->model_vaksinasi->store($save_data);
+
+			if ($save_vaksinasi) {
+				if ($this->input->post('save_type') == 'stay') {
+					$this->data['success'] = true;
+					$this->data['id'] 	   = $save_vaksinasi;
+					$this->data['message'] = cclang('success_save_data_stay', [
+						anchor('vaksinasi/edit/' . $save_vaksinasi, 'Edit Vaksinasi'),
+						anchor('vaksinasi', ' Go back to list')
+					]);
+				} else {
+					set_message(
+						cclang('success_save_data_redirect', [
+						anchor('vaksinasi/edit/' . $save_vaksinasi, 'Edit Vaksinasi')
+					]), 'success');
+
+            		$this->data['success'] = true;
+					$this->data['redirect'] = base_url('vaksinasi');
+				}
+			} else {
+				if ($this->input->post('save_type') == 'stay') {
+					$this->data['success'] = false;
+					$this->data['message'] = cclang('data_not_change');
+				} else {
+            		$this->data['success'] = false;
+            		$this->data['message'] = cclang('data_not_change');
+					$this->data['redirect'] = base_url('vaksinasi');
+				}
+			}
+
+		} else {
+			$this->data['success'] = false;
+			$this->data['message'] = validation_errors();
+		}
+
+		echo json_encode($this->data);
+	}
+	
+		/**
+	* Update view Vaksinasis
+	*
+	* @var $id String
+	*/
+	public function edit($id)
+	{
+		$this->is_allowed('vaksinasi_update');
+
+		$this->data['vaksinasi'] = $this->model_vaksinasi->find($id);
+
+		$this->template->title('Vaksinasi Update');
+		$this->render('modul/vaksinasi/vaksinasi_update', $this->data);
+	}
+
+	/**
+	* Update Vaksinasis
+	*
+	* @var $id String
+	*/
+	public function edit_save($id)
+	{
+		if (!$this->is_allowed('vaksinasi_update', false)) {
+			echo json_encode([
+				'success' => false,
+				'message' => cclang('sorry_you_do_not_have_permission_to_access')
+				]);
+			exit;
+		}
+		
+		$this->form_validation->set_rules('dosis', 'Dosis', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('jenis_vaksin', 'Jenis Vaksin', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('faskes', 'Faskes', 'trim|required|max_length[100]');
+		$this->form_validation->set_rules('tiket', 'Tiket', 'trim|required|max_length[50]');
+		$this->form_validation->set_rules('tanggal', 'Tanggal', 'trim|required');
+		
+		if ($this->form_validation->run()) {
+		
+			$save_data = [
+				'dosis' => $this->input->post('dosis'),
+				'jenis_vaksin' => $this->input->post('jenis_vaksin'),
+				'faskes' => $this->input->post('faskes'),
+				'tiket' => $this->input->post('tiket'),
+				'tanggal' => $this->input->post('tanggal'),
+				'last_updated_by' => get_user_data('username'),
+				'last_updated_date' => date('Y-m-d H:i:s'),
+			];
+
+			
+			$save_vaksinasi = $this->model_vaksinasi->change($id, $save_data);
+
+			if ($save_vaksinasi) {
+				if ($this->input->post('save_type') == 'stay') {
+					$this->data['success'] = true;
+					$this->data['id'] 	   = $id;
+					$this->data['message'] = cclang('success_update_data_stay', [
+						anchor('vaksinasi', ' Go back to list')
+					]);
+				} else {
+					set_message(
+						cclang('success_update_data_redirect', [
+					]), 'success');
+
+            		$this->data['success'] = true;
+					$this->data['redirect'] = base_url('vaksinasi');
+				}
+			} else {
+				if ($this->input->post('save_type') == 'stay') {
+					$this->data['success'] = false;
+					$this->data['message'] = cclang('data_not_change');
+				} else {
+            		$this->data['success'] = false;
+            		$this->data['message'] = cclang('data_not_change');
+					$this->data['redirect'] = base_url('vaksinasi');
+				}
+			}
+		} else {
+			$this->data['success'] = false;
+			$this->data['message'] = validation_errors();
+		}
+
+		echo json_encode($this->data);
+	}
+	
+	/**
+	* delete Vaksinasis
+	*
+	* @var $id String
+	*/
+	public function delete($id = null)
+	{
+		$this->is_allowed('vaksinasi_delete');
+
+		$this->load->helper('file');
+
+		$arr_id = $this->input->get('id');
+		$remove = false;
+
+		if (!empty($id)) {
+			$remove = $this->_remove($id);
+		} elseif (count($arr_id) >0) {
+			foreach ($arr_id as $id) {
+				$remove = $this->_remove($id);
+			}
+		}
+
+		if ($remove) {
+            set_message(cclang('has_been_deleted', 'vaksinasi'), 'success');
+        } else {
+            set_message(cclang('error_delete', 'vaksinasi'), 'error');
+        }
+
+		redirect_back();
+	}
+
+		/**
+	* View view Vaksinasis
+	*
+	* @var $id String
+	*/
+	public function view($id)
+	{
+		$this->is_allowed('vaksinasi_view');
+
+		$this->data['vaksinasi'] = $this->model_vaksinasi->join_avaiable()->filter_avaiable()->find($id);
+
+		$this->template->title('Vaksinasi Detail');
+		$this->render('modul/vaksinasi/vaksinasi_view', $this->data);
+	}
+	
+	/**
+	* delete Vaksinasis
+	*
+	* @var $id String
+	*/
+	private function _remove($id)
+	{
+		$vaksinasi = $this->model_vaksinasi->find($id);
+
+		
+		
+		return $this->model_vaksinasi->remove($id);
+	}
+	
+	
+	/**
+	* Export to excel
+	*
+	* @return Files Excel .xls
+	*/
+	public function export()
+	{
+		$this->is_allowed('vaksinasi_export');
+
+		$this->model_vaksinasi->export('vaksinasi', 'vaksinasi');
+	}
+
+	/**
+	* Export to PDF
+	*
+	* @return Files PDF .pdf
+	*/
+	public function export_pdf()
+	{
+		$this->is_allowed('vaksinasi_export');
+
+		$this->model_vaksinasi->pdf('vaksinasi', 'vaksinasi');
+	}
+}
+
+
+/* End of file vaksinasi.php */
+/* Location: ./application/controllers/Vaksinasi.php */
---
+++ application/views/modul/vaksinasi/vaksinasi_update.php
@@ -0,0 +1,213 @@
+
+<script src="<?= BASE_ASSET; ?>/js/jquery.hotkeys.js"></script>
+<script type="text/javascript">
+    function domo(){
+     
+       // Binding keys
+       $('*').bind('keydown', 'Ctrl+s', function assets() {
+          $('#btn_save').trigger('click');
+           return false;
+       });
+    
+       $('*').bind('keydown', 'Ctrl+x', function assets() {
+          $('#btn_cancel').trigger('click');
+           return false;
+       });
+    
+      $('*').bind('keydown', 'Ctrl+d', function assets() {
+          $('.btn_save_back').trigger('click');
+           return false;
+       });
+        
+    }
+    
+    jQuery(document).ready(domo);
+</script>
+
+<!-- Main content -->
+<section class="content">
+    <div class="row" >
+        <div class="col-md-12">
+            <div class="box box-warning">
+                <div class="box-body ">
+                    <!-- Widget: user widget style 1 -->
+                    <div class="box box-widget widget-user-2">
+                        <!-- Add the bg color to the header using any of the bg-* classes -->
+                        <div class="widget-user-header ">
+                            <div class="widget-user-image">
+                                <img class="img-circle" src="<?= BASE_ASSET; ?>/img/add2.png" alt="User Avatar">
+                            </div>
+                            <!-- /.widget-user-image -->
+                            <h3 class="widget-user-username"><b>Vaksinasi</b></h3>
+                            <h5 class="widget-user-desc">Edit Vaksinasi</h5>
+                            <hr>
+                        </div>
+                        <?= form_open(base_url('vaksinasi/edit_save/'.$this->uri->segment(3)), [
+                            'name'    => 'form_vaksinasi', 
+                            'class'   => 'form-horizontal', 
+                            'id'      => 'form_vaksinasi', 
+                            'method'  => 'POST'
+                            ]); ?>
+                         
+                                                <div class="form-group ">
+                            <label for="dosis" class="col-sm-2 control-label">Dosis 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="dosis" id="dosis" placeholder="Dosis" value="<?= set_value('dosis', $vaksinasi->dosis); ?>">
+                                <small class="info help-block">
+                                <b>Input Dosis</b> Max Length : 50.</small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="jenis_vaksin" class="col-sm-2 control-label">Jenis Vaksin 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="jenis_vaksin" id="jenis_vaksin" placeholder="Jenis Vaksin" value="<?= set_value('jenis_vaksin', $vaksinasi->jenis_vaksin); ?>">
+                                <small class="info help-block">
+                                <b>Input Jenis Vaksin</b> Max Length : 50.</small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="faskes" class="col-sm-2 control-label">Faskes 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="faskes" id="faskes" placeholder="Faskes" value="<?= set_value('faskes', $vaksinasi->faskes); ?>">
+                                <small class="info help-block">
+                                <b>Input Faskes</b> Max Length : 100.</small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="tiket" class="col-sm-2 control-label">Tiket 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="tiket" id="tiket" placeholder="Tiket" value="<?= set_value('tiket', $vaksinasi->tiket); ?>">
+                                <small class="info help-block">
+                                <b>Input Tiket</b> Max Length : 50.</small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="tanggal" class="col-sm-2 control-label">Tanggal 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                            <div class="input-group date col-sm-8">
+                              <input type="text" class="form-control pull-right datepicker" name="tanggal"  placeholder="Tanggal" id="tanggal" value="<?= set_value('vaksinasi_tanggal_name', $vaksinasi->tanggal); ?>">
+                            </div>
+                            <small class="info help-block">
+                            </small>
+                            </div>
+                        </div>
+                       
+                                                 
+                         
+                        
+                        <div class="message"></div>
+                        <div class="row-fluid col-md-7">
+                            <button class="btn btn-flat btn-primary btn_save btn_action" id="btn_save" data-stype='stay' title="<?= cclang('save_button'); ?> (Ctrl+s)">
+                            <i class="fa fa-save" ></i> <?= cclang('save_button'); ?>
+                            </button>
+                            <a class="btn btn-flat btn-info btn_save btn_action btn_save_back" id="btn_save" data-stype='back' title="<?= cclang('save_and_go_the_list_button'); ?> (Ctrl+d)">
+                            <i class="ion ion-ios-list-outline" ></i> <?= cclang('save_and_go_the_list_button'); ?>
+                            </a>
+                            <a class="btn btn-flat btn-default btn_action" id="btn_cancel" title="<?= cclang('cancel_button'); ?> (Ctrl+x)">
+                            <i class="fa fa-undo" ></i> <?= cclang('cancel_button'); ?>
+                            </a>
+                            <span class="loading loading-hide">
+                            <img src="<?= BASE_ASSET; ?>/img/loading-spin-primary.svg"> 
+                            <i><?= cclang('loading_saving_data'); ?></i>
+                            </span>
+                        </div>
+                        <?= form_close(); ?>
+                    </div>
+                </div>
+                <!--/box body -->
+            </div>
+            <!--/box -->
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+<!-- Page script -->
+<script>
+    $(document).ready(function(){
+      
+             
+      $('#btn_cancel').click(function(){
+        swal({
+            title: "Are you sure?",
+            text: "the data that you have created will be in the exhaust!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes!",
+            cancelButtonText: "No!",
+            closeOnConfirm: true,
+            closeOnCancel: true
+          },
+          function(isConfirm){
+            if (isConfirm) {
+              window.location.href = BASE_URL + 'vaksinasi';
+            }
+          });
+    
+        return false;
+      }); /*end btn cancel*/
+    
+      $('.btn_save').click(function(){
+        $('.message').fadeOut();
+            
+        var form_vaksinasi = $('#form_vaksinasi');
+        var data_post = form_vaksinasi.serializeArray();
+        var save_type = $(this).attr('data-stype');
+        data_post.push({name: 'save_type', value: save_type});
+    
+        $('.loading').show();
+    
+        $.ajax({
+          url: form_vaksinasi.attr('action'),
+          type: 'POST',
+          dataType: 'json',
+          data: data_post,
+        })
+        .done(function(res) {
+          if(res.success) {
+            var id = $('#vaksinasi_image_galery').find('li').attr('qq-file-id');
+            if (save_type == 'back') {
+              window.location.href = res.redirect;
+              return;
+            }
+    
+            $('.message').printMessage({message : res.message});
+            $('.message').fadeIn();
+            $('.data_file_uuid').val('');
+    
+          } else {
+            $('.message').printMessage({message : res.message, type : 'warning'});
+          }
+    
+        })
+        .fail(function() {
+          $('.message').printMessage({message : 'Error save data', type : 'warning'});
+        })
+        .always(function() {
+          $('.loading').hide();
+          $('html, body').animate({ scrollTop: $(document).height() }, 2000);
+        });
+    
+        return false;
+      }); /*end btn save*/
+      
+       
+       
+           
+    
+    }); /*end doc ready*/
+</script>
